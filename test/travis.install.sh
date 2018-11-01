d622d6ee07d0ced2743ffeaeb0b10523b5268612
Stefano Tamagnini <yoghi@sigmalab.net>
2018-11-01 02:11:49 +0100

    clean way config ssh

--- test/travis.install.sh
+++ test/travis.install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# config ssh
+echo "Host 127.0.0.1" > ~/.ssh/config
+echo "\tStrictHostKeyChecking no" >> ~/.ssh/config
+echo "\tUserKnownHostsFile=/dev/null" >> ~/.ssh/config
+
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew update
     brew install qemu

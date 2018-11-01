f98fab0a03236b78b36daf37c5f1691c1f332911
Stefano Tamagnini <yoghi@sigmalab.net>
2018-11-01 02:02:00 +0100

    wput missing

--- test/travis.install.sh
+++ test/travis.install.sh
@@ -3,9 +3,10 @@
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew update
     brew install qemu
+    brew install wput
 fi
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     sudo apt-get update
-    sudo apt-get install expect qemu-system
+    sudo apt-get install expect qemu-system wput
 fi

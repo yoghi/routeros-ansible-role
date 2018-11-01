44ecf199a74d1a6d97ada940260768c19f2ac803
Stefano Tamagnini <yoghi@sigmalab.net>
2018-11-01 14:47:35 +0100

    silent install

--- test/travis.install.sh
+++ test/travis.install.sh
@@ -13,6 +13,6 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 fi
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-    sudo apt-get update
-    sudo apt-get install expect qemu-system wput
+    sudo apt-get update -qq
+    sudo apt-get install expect qemu-system wput -qq
 fi
